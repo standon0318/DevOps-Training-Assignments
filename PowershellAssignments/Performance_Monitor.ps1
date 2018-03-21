65a07cdd4ce60acd865ec4be078be52863fa7755
standon0318 <standon@tavisca.com>
2018-03-21 09:45:31 +0530

    [add]Powershell basic assignments

---
+++ PowershellAssignments/Performance_Monitor.ps1
@@ -0,0 +1,7 @@
+﻿
+echo ($DiskSpace = Get-Counter -counter "\LogicalDisk(_Total)\% Free Space").CounterSamples.CookedValue
+echo $CPUTime=(Get-Counter -counter "\Processor(_Total)\% Processor Time”).CounterSamples.CookedValue
+
+echo $AvailableMBytes=(Get-Counter -counter "\Memory\Available MBytes”).CounterSamples.CookedValue
+echo "Processor Performance="(Get-Counter -counter '\TCPv6\Connections Active').CounterSamples.CookedValue
+echo "File Write Operations="(Get-Counter -counter '\System\File Write Operations/sec').CounterSamples.CookedValue
