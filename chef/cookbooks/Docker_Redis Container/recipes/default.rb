621c9e67dc4493d52d7e0efcf0883508c49fd8c9
shashank <standon@tavisca.com>
2018-04-09 19:00:03 +0530

    [added] docker cookbk

--- chef/cookbooks/Docker_Redis Container/recipes/default.rb
+++ chef/cookbooks/Docker_Redis Container/recipes/default.rb
@@ -10,11 +10,12 @@
 package node['Docker_Redis Container']['package']['name'] do
 	version node['Docker_Redis Container']['package']['version']
 	action :install
+	flush_cache [ :before, :after]
 end
 
 #Starting docker service
-service ['Docker_Redis Container']['service']['name'] do
-	action [:enable, :start]
+service node['Docker_Redis Container']['service']['name'] do
+	action [ :enable, :start]
 end
 
 #pulling redis-container for docker 
@@ -26,6 +27,7 @@ end
 #install redis from redis image
 docker_container node['Docker_Redis Container']['docker_container']['name'] do
 	image node['Docker_Redis Container']['docker_container']['image']
+	tag node['Docker_Redis Container']['docker_container']['tag']	
 	container_name node['Docker_Redis Container']['docker_container']['container_name']
 	detach node['Docker_Redis Container']['docker_container']['detach']
 	port node['Docker_Redis Container']['docker_container']['port']
